8c10ec5c2c0dd36f169653748199f0abc888bf0d
dirksavage88 <dirksavage88@gmail.com>
2023-05-23 19:48:59 -0400

    beaglebone 1.13
    Signed-off-by: dirksavage88 <dirksavage88@gmail.com>

--- boards/beaglebone/blue/cmake/upload.cmake
+++ boards/beaglebone/blue/cmake/upload.cmake
@@ -34,7 +34,7 @@
 add_custom_target(upload
 	COMMAND rsync -arh --progress
 			${CMAKE_RUNTIME_OUTPUT_DIRECTORY} ${PX4_SOURCE_DIR}/posix-configs/bbblue/*.config ${PX4_BINARY_DIR}/etc # source
-			debian@beaglebone.lan:/home/debian/px4 # destination
+			savagerobots2@192.168.1.41:/home/debian/px4 # destination
 	DEPENDS px4
 	COMMENT "uploading px4"
 	USES_TERMINAL
